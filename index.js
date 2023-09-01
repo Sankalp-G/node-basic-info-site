378ce56d4908208bfb39c73e2adfbae241ee9c8d
Sankalp-G <sankalp.gawali@gmail.com>
2023-09-01 22:35:23 +0530

    add about and contact-me pages and routes

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  fs.readFile('./pages/index.html', (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.write(data);
-    res.end()
-  })
+
+  if (req.url == '/') {
+    fs.readFile('./pages/index.html', (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.end(data)
+    })
+  }
+  else if (req.url == '/about') {
+    const html = fs.readFileSync('./pages/about.html')
+    res.end(html)
+  }
+  else if (req.url == '/contact-me') {
+    const html = fs.readFileSync('./pages/contact-me.html')
+    res.end(html)
+  }
 });
 
 server.listen(port, hostname, () => {
